19acae5faee6c18bc2bb5d0a98afc3388d02699b
Leila.L <leila.lababsa@hotmail.fr>
2017-09-06 11:51:51 +0200

    check indentation

--- secret.php
+++ secret.php
@@ -1,19 +1,10 @@
 <?php
 
-if (isset($_POST['mdp']) AND $_POST['mdp'] == 'kangourou'   )
-
+if (isset($_POST['mdp']) and $_POST['mdp'] == 'kangourou')
 {
-
-  echo '<h1>Confidential Information</h1><br/> Neil A. Armstrong n\'est pas allé sur la lune.';
-
+    echo '<h1>Confidential Information</h1><br/> Neil A. Armstrong n\'est pas allé sur la lune.';
 }
-
 else
-
 {
-
     echo 'Mauvais <em>mot de passe</em> !<br/> Pour réessayer <a href="index.php">clique</a>';
-
 }
-
-?>
