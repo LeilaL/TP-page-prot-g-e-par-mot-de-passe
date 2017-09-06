d858d682c869f2c6e1a994f1a5b3e04aa66dd00e
Leila.L <leila.lababsa@hotmail.fr>
2017-09-06 11:04:43 +0200

    added php condition

--- secret.php
+++ secret.php
@@ -1,1 +1,19 @@
-<?php echo "Neil A. Armstrongn n'est pas allé sur la lune."; ?>
+<?php
+
+if (isset($_POST['prenom']) )
+
+{
+
+  echo "Neil A. Armstrongn n'est pas allé sur la lune.";
+
+}
+
+else
+
+{
+
+    echo 'Mauvais mot de passe';
+
+}
+
+?>
